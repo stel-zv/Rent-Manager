1bfb873261091809b9a1f4de906eba8e6abf6cd3
szaborov <stella.zaborov@epfedu.fr>
2023-04-27 14:24:35 +0200

    suppression vehicle

--- rentmanager/src/main/java/com/epf/rentmanager/servlet/UserDeleteServlet.java
+++ rentmanager/src/main/java/com/epf/rentmanager/servlet/UserDeleteServlet.java
@@ -1,8 +1,7 @@
 package com.epf.rentmanager.servlet;
 
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;
+
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -11,9 +10,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import com.epf.rentmanager.exception.ServiceException;
-import com.epf.rentmanager.model.Client;
 import com.epf.rentmanager.service.ClientService;
-import com.epf.rentmanager.service.VehicleService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.context.support.SpringBeanAutowiringSupport;
 
--- rentmanager/src/main/java/com/epf/rentmanager/servlet/VehicleDeleteServlet.java
+++ rentmanager/src/main/java/com/epf/rentmanager/servlet/VehicleDeleteServlet.java
@@ -1,4 +1,55 @@
 package com.epf.rentmanager.servlet;
 
-public class VehicleDeleteServlet {
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.epf.rentmanager.exception.ServiceException;
+import com.epf.rentmanager.service.VehicleService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.context.support.SpringBeanAutowiringSupport;
+
+
+@WebServlet(name = "VehicleDeleteServlet", urlPatterns = "/cars/delete")
+public class VehicleDeleteServlet extends HttpServlet {
+
+    @Autowired
+
+    VehicleService vehicleService;
+
+    /**
+     *
+     */
+
+    @Override
+    public void init() throws ServletException{
+        super.init();
+        SpringBeanAutowiringSupport.processInjectionBasedOnCurrentContext(this);
+    }
+
+    private static final long serialVersionUID = 1L;
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        try{
+            int vehicleId = Integer.parseInt(request.getParameter("id"));
+            vehicleService.delete(vehicleId);
+        }
+        catch (ServiceException e) {
+            e.printStackTrace();
+        }
+        response.sendRedirect("/rentmanager/cars");
+
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        doGet(request, response);
+    }
+
 }
