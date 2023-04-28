42471fdfc5bd9efcb386515ad4930aecc71dd855
stel-zv <123560618+stel-zv@users.noreply.github.com>
2023-04-28 16:42:12 +0200

    clean main

--- rentmanager/src/main/java/com/epf/rentmanager/main/main.java
+++ rentmanager/src/main/java/com/epf/rentmanager/main/main.java
@@ -25,58 +25,5 @@ public class main {
         VehicleService vehicleService = context.getBean(VehicleService.class);
         ReservationService reservationService = context.getBean(ReservationService.class);
 
-        Client stella = null;
-        clientService.create(stella);
-
-        // ReservationService reservationService = context.getBean(ReservationService.class);
-
-        //Date
-        // DateTimeFormatter formatter = DateTimeFormatter.ofPattern("d/MM/yyyy");
-            /*String date = "16/09/2001";
-
-            //Ajout et suppression client
-            LocalDate naissancestella = LocalDate.parse(date, formatter);
-            Client stella = new Client ("zaborov", "stella", "stella.zaborov@gmail.com",naissancestella);
-
-            //Montrer tous les clients
-            List<Client> clients = clientService.findAll();
-            System.out.println(clients);
-
-            //Montrer un seul client
-            Client client = clientService.findById(4);
-            //System.out.println(client);
-
-            //Ajout et suppression vehicule
-            Vehicle yaris = new Vehicle("Toyota",4);
-                //VehicleService.getInstance().create(yaris);
-              //VehicleService.getInstance().delete(6);
-
-            //Montrer tous les vehicules
-            List<Vehicle> vehicles = vehicleService.findAll();
-            //System.out.println(vehicles);
-
-            //Montrer un seul vehicule
-            Vehicle vehicle = vehicleService.findById(2);
-                //System.out.println(vehicle);
-
-            //Ajout reservation
-                //Reservation reservation = new Reservation(1,1,naissancestella,naissancestella);
-                //ReservationService.getInstance().create(reservation);
-                //List <Reservation> resas = ReservationService.getInstance().findAll();
-                //System.out.println (resas);
-                //System.out.println (resas.size());
-                //System.out.println(ReservationService.getInstance().findResaById(1));
-                //System.out.println(ReservationService.getInstance().findResaByVehicleId(1));
-                //System.out.println(ReservationService.getInstance().findResaByClientId(1));
-
-            //counter clients
-            int count_clients = clientService.findAll().size();
-            int count_vehicles = vehicleService.findAll().size();
-           // System.out.println (count_clients);
-            //System.out.println (count_vehicles);
-
-           List<Vehicle> vehiclesss = reservationService.findVehicleResaByClientId(1);
-
-*/
     }
 }
